8ace985b08cdb7ed0ed046c801eb090a79b8175c
Jess Rees <jessrees@gmail.com>
2016-07-26 00:56:15 -0400

    clickable constellations

---
+++ build/constellationData.js
@@ -0,0 +1,213 @@
+var colors = [
+	"#9370DB", 
+	"#FFD700",
+	"#FF6347",
+	"#87CEEB",
+	"#2BADD9",
+	"#DADDAA",
+	"#00FF00",
+	"#40E0D0",
+	"#FFFF00",
+	"#CD5C5C",
+	"#E6E6FA",
+	"#FF00FF"
+];
+
+var aquarius = [];
+var aries = [];
+var cancer = [];
+var capricorn = [];
+var gemini = [];
+var leo = [];
+var libra = [];
+var pisies = [];
+var sagitarius = [];
+var scorpio = [];
+var taurus = [];
+var virgo = [];
+
+var zodiac = [
+		aquarius,
+		aries,
+		cancer,
+		capricorn,
+		gemini,
+		leo,
+		libra,
+		pisies,
+		sagitarius,
+		scorpio,
+		taurus,
+		virgo
+	];
+
+var zodiacNames = [
+		"aquarius",
+		"aries",
+		"cancer",
+		"capricorn",
+		"gemini",
+		"leo",
+		"libra",
+		"pisies",
+		"sagitarius",
+		"scorpio",
+		"taurus",
+		"virgo"
+	];
+
+aquarius.push( new THREE.Vector4 (665.458, -333.932, 140.153, -3.88));
+aquarius.push( new THREE.Vector4 (485.127, -360.351, 91.5421, -3.47));
+aquarius.push( new THREE.Vector4 (143.277, -61.6656, 22.5735, 0.437));
+aquarius.push( new THREE.Vector4 (147.152, -56.8603, -22.7101, -0.178));
+aquarius.push( new THREE.Vector4 (151.065, -169.414, 32.2198, -0.457));
+aquarius.push( new THREE.Vector4 (95.2547, -36.7388, 15.8871, 1.14));
+aquarius.push( new THREE.Vector4 (170.968, -60.8679, 25.9775, 0.288));
+aquarius.push( new THREE.Vector4 (170.551, -85.9182, 9.02696, 0.327));
+aquarius.push( new THREE.Vector4 (147.216, -89.4378, -6.26216, 0.672));
+aquarius.push( new THREE.Vector4 (371.034, -123.599, -2.64379, -1.67));
+aquarius.push( new THREE.Vector4 (1007.64, -394.93, 200.041, -2.84));
+aquarius.push( new THREE.Vector4 (216.315, -49.3892, -4.07582, 0.0536));
+aquarius.push( new THREE.Vector4 (143.717, -35.0133, -10.3481, 0.949));
+aquarius.push( new THREE.Vector4 (239.388, -96.7464, -26.7757, 1.17));
+aquarius.push( new THREE.Vector4 (64.5582, -33.5461, -14.0121, 3.43));
+aries.push( new THREE.Vector4 (51.3345, 39.6212, 11.3956, 0.482));
+aries.push( new THREE.Vector4 (48.8159, 32.8891, 8.78515, 1.33));
+aries.push( new THREE.Vector4 (169.498, 110.85, 25.4553, -0.105));
+aries.push( new THREE.Vector4 (105.743, 129.897, 5.339, 0.793));
+aries.push( new THREE.Vector4 (193.095, 218.24, 21.2773, -0.134));
+cancer.push( new THREE.Vector4 (-160.673, 235.916, -51.7985, -1.22));
+cancer.push( new THREE.Vector4 (-85.0172, 106.036, 0.188335, 0.838));
+cancer.push( new THREE.Vector4 (-173.572, 235.889, 53.9008, -0.775));
+cancer.push( new THREE.Vector4 (-119.186, 124.976, -15.3485, 0.63));
+cancer.push( new THREE.Vector4 (-96.3009, 125.326, 8.81534, 1.23));
+capricorn.push( new THREE.Vector4 (30.9472, -22.8641, -1.74874, 2.49));
+capricorn.push( new THREE.Vector4 (191.784, -283.824, 27.4813, -2.07));
+capricorn.push( new THREE.Vector4 (60.0394, -89.4885, 13.0949, 0.966));
+capricorn.push( new THREE.Vector4 (108.913, -85.7333, -6.19398, 0.543));
+capricorn.push( new THREE.Vector4 (288.456, -269.577, -48.4259, -1.66));
+capricorn.push( new THREE.Vector4 (109.486, -113.997, -1.61999, 0.65));
+capricorn.push( new THREE.Vector4 (381.388, -488.861, -97.8086, -2.3));
+capricorn.push( new THREE.Vector4 (28.6536, -37.8049, -5.84975, 3.3));
+capricorn.push( new THREE.Vector4 (159.167, -144.919, -5.14195, 0.179));
+capricorn.push( new THREE.Vector4 (378.394, -565.899, 83.4254, -2.32));
+gemini.push( new THREE.Vector4 (-13.1852, 30.7379, 3.92108, 1.09));
+gemini.push( new THREE.Vector4 (-17.5378, 47.562, 9.02799, 0.586));
+gemini.push( new THREE.Vector4 (-19.4192, 56.1158, 10.2689, 2.82));
+gemini.push( new THREE.Vector4 (-57.4477, 131.081, 7.7024, 0.353));
+gemini.push( new THREE.Vector4 (-87.0174, 222.678, 21.8345, -0.277));
+gemini.push( new THREE.Vector4 (-40.6564, 118.359, 12.625, 0.846));
+gemini.push( new THREE.Vector4 (-79.5204, 287.826, 40.6741, -0.421));
+gemini.push( new THREE.Vector4 (-37.1844, 189.119, 37.5781, -0.301));
+gemini.push( new THREE.Vector4 (-18.6609, 55.7089, -0.180862, 2.22));
+gemini.push( new THREE.Vector4 (-30.1725, 88.7383, -9.24467, 1.27));
+gemini.push( new THREE.Vector4 (-301.829, 1127.21, -41.4998, -3.76));
+gemini.push( new THREE.Vector4 (-10.9343, 55.1751, -10.0209, 2.13));
+gemini.push( new THREE.Vector4 (-16.4504, 102.651, -12.2867, -0.605));
+gemini.push( new THREE.Vector4 (-155.653, 888.304, 32.6298, -4.15));
+gemini.push( new THREE.Vector4 (-21.3921, 230.526, -3.30431, -1.39));
+gemini.push( new THREE.Vector4 (-59.3717, 497.725, -26.7447, -1.81));
+gemini.push( new THREE.Vector4 (-20.904, 348.127, -5.39348, -1.84));
+leo.push( new THREE.Vector4 (-66.9131, 38.8973, 0.629854, -0.519));
+leo.push( new THREE.Vector4 (-106.975, 62.7264, 19.233, -0.918));
+leo.push( new THREE.Vector4 (-34.931, 5.14667, 7.67753, 1.92));
+leo.push( new THREE.Vector4 (-52.902, 17.8894, 14.2703, 1.32));
+leo.push( new THREE.Vector4 (-191.008, 156.309, 42.2631, -1.46));
+leo.push( new THREE.Vector4 (-167.644, 49.9043, 29.8199, -0.351));
+leo.push( new THREE.Vector4 (-214.239, 136.137, 53.3354, -1.08));
+leo.push( new THREE.Vector4 (-1797.34, 1127.25, 180.647, -5.6));
+leo.push( new THREE.Vector4 (-109.376, 78.7474, -8.84719, 0.432));
+leo.push( new THREE.Vector4 (-101.475, 80.9168, 28.418, 0.828));
+leo.push( new THREE.Vector4 (-76.6674, 16.9047, 8.39815, 2.08));
+leo.push( new THREE.Vector4 (-246.987, 223.376, 46.1523, -0.748));
+leo.push( new THREE.Vector4 (-149.034, 147.5, 38.5892, 0.391));
+libra.push( new THREE.Vector4 (-102.943, -119.99, 23.6125, -0.844));
+libra.push( new THREE.Vector4 (-54.4429, -54.6007, 0.446571, 0.879));
+libra.push( new THREE.Vector4 (-183.133, -223.648, -38.8042, -1.51));
+libra.push( new THREE.Vector4 (-100.13, -164.102, -28.7619, -0.279));
+libra.push( new THREE.Vector4 (-223.109, -376.524, -77.3489, -2.01));
+libra.push( new THREE.Vector4 (-86.6804, -124.429, 11.6261, 0.564));
+pisies.push( new THREE.Vector4 (119.547, 67.3034, -21.8756, 0.671));
+pisies.push( new THREE.Vector4 (128.241, -19.2765, 16.5144, 0.683));
+pisies.push( new THREE.Vector4 (295.293, 74.4308, 11.5779, -0.415));
+pisies.push( new THREE.Vector4 (181.213, 57.2247, 3.62998, 0.44));
+pisies.push( new THREE.Vector4 (261.013, 131.937, 27.5359, -1.16));
+pisies.push( new THREE.Vector4 (156.087, -13.1356, 24.9012, 0.833));
+pisies.push( new THREE.Vector4 (44.541, -1.83239, 5.59611, 3.43));
+pisies.push( new THREE.Vector4 (330.744, 157.805, -30.0851, -0.813));
+pisies.push( new THREE.Vector4 (228.196, 120.024, -7.2888, -0.233));
+pisies.push( new THREE.Vector4 (133.371, 71.8628, 57.3684, 1.03));
+pisies.push( new THREE.Vector4 (259.629, 142.658, 93.2142, -0.156));
+pisies.push( new THREE.Vector4 (325.279, 161.886, 100.791, -0.647));
+pisies.push( new THREE.Vector4 (105.081, 4.74059, 11.7308, 1.47));
+sagitarius.push( new THREE.Vector4 (12.5525, -141.232, -27.6986, -1.44));
+sagitarius.push( new THREE.Vector4 (47.9701, -218.444, -13.4889, -2.14));
+sagitarius.push( new THREE.Vector4 (20.8171, -85.7984, -11.1235, 0.418));
+sagitarius.push( new THREE.Vector4 (24.2293, -302.404, -34.4269, -2.14));
+sagitarius.push( new THREE.Vector4 (8.48872, -76.6762, -2.87899, 0.947));
+sagitarius.push( new THREE.Vector4 (122.997, -421.946, 11.0115, -2.77));
+sagitarius.push( new THREE.Vector4 (2.09732, -95.2492, -11.686, 0.634));
+sagitarius.push( new THREE.Vector4 (9.16999, -144.627, -34.4697, -0.201));
+sagitarius.push( new THREE.Vector4 (40.6263, -226.226, -15.8953, -1.08));
+sagitarius.push( new THREE.Vector4 (30.6684, -115.79, -10.67, 0.484));
+sagitarius.push( new THREE.Vector4 (86.4743, -361.537, 10.7677, -1.77));
+sagitarius.push( new THREE.Vector4 (35.8754, -133.951, 2.07708, 0.614));
+sagitarius.push( new THREE.Vector4 (40.382, -114.353, 8.9525, 1.06));
+sagitarius.push( new THREE.Vector4 (95.1711, -336.862, -142.472, -1.36));
+sagitarius.push( new THREE.Vector4 (46.0978, -154.278, -53.5069, 0.377));
+sagitarius.push( new THREE.Vector4 (67.8241, -163.286, -66.6747, 0.303));
+sagitarius.push( new THREE.Vector4 (34.9092, -123.114, -53.0037, 1.13));
+sagitarius.push( new THREE.Vector4 (251.351, -542.227, -153.312, -2.02));
+scorpio.push( new THREE.Vector4 (-210.067, -569.774, -48.5596, -5.28));
+scorpio.push( new THREE.Vector4 (-64.3456, -678.823, -167.362, -5.05));
+scorpio.push( new THREE.Vector4 (-19.6334, -255.134, -91.3554, -2.75));
+scorpio.push( new THREE.Vector4 (-184.698, -355.89, -13.9159, -3.16));
+scorpio.push( new THREE.Vector4 (-16.1969, -61.9005, -13.2968, 0.778));
+scorpio.push( new THREE.Vector4 (-27.4831, -445.392, -124.982, -3.38));
+scorpio.push( new THREE.Vector4 (-238.886, -472.705, 9.29909, -3.5));
+scorpio.push( new THREE.Vector4 (-52.4213, -499.781, -125.389, -3.31));
+scorpio.push( new THREE.Vector4 (-135.725, -404.612, -45.7779, -2.78));
+scorpio.push( new THREE.Vector4 (-207.783, -406.747, -43.7951, -2.85));
+scorpio.push( new THREE.Vector4 (-276.557, -677.647, -51.6863, -3.86));
+scorpio.push( new THREE.Vector4 (-74.0987, -1712.74, -514.865, -5.71));
+scorpio.push( new THREE.Vector4 (-189.287, -768.056, -218.261, -4.01));
+scorpio.push( new THREE.Vector4 (-10.7972, -66.2393, -24.673, 1.61));
+scorpio.push( new THREE.Vector4 (-31.2765, -138.087, -50.5423, 0.299));
+scorpio.push( new THREE.Vector4 (-182.563, -360.576, -61.1293, -1.62));
+scorpio.push( new THREE.Vector4 (-187.655, -379.19, 1.61673, -1.64));
+scorpio.push( new THREE.Vector4 (-186.69, -393.939, 12.4158, -1.63));
+scorpio.push( new THREE.Vector4 (-37.5643, -68.6304, 12.714, 2.91));
+taurus.push( new THREE.Vector4 (16.8399, 129.216, 12.2895, -1.37));
+taurus.push( new THREE.Vector4 (37.8407, 414.565, -15.9445, -2.56));
+taurus.push( new THREE.Vector4 (22.367, 60.7565, -6.1939, -0.631));
+taurus.push( new THREE.Vector4 (183.224, 317.255, 25.9582, -2.41));
+taurus.push( new THREE.Vector4 (58.8872, 145.423, -15.7789, 0.416));
+taurus.push( new THREE.Vector4 (55.5532, 137.235, -15.1339, 0.101));
+taurus.push( new THREE.Vector4 (62.7527, 139.667, -15.3647, 0.279));
+taurus.push( new THREE.Vector4 (59.9632, 140.382, -10.5873, 0.411));
+taurus.push( new THREE.Vector4 (56.482, 133.896, -10.4413, 1.55));
+taurus.push( new THREE.Vector4 (56.7792, 143.884, -6.92959, 0.145));
+taurus.push( new THREE.Vector4 (127.291, 261.161, -6.91435, 0.758));
+taurus.push( new THREE.Vector4 (56.866, 142.205, 1.68139, 0.849));
+taurus.push( new THREE.Vector4 (56.6552, 143.831, 2.989, 0.898));
+taurus.push( new THREE.Vector4 (122.643, 380.929, 5.00125, -1.18));
+taurus.push( new THREE.Vector4 (179.585, 319.163, -51.1943, -1.87));
+taurus.push( new THREE.Vector4 (130.731, 162.389, -34.2599, -0.45));
+taurus.push( new THREE.Vector4 (135.285, 172.61, -33.9384, -0.436));
+taurus.push( new THREE.Vector4 (62.6467, 108.155, -32.2076, 0.92));
+taurus.push( new THREE.Vector4 (188.91, 380.247, -91.6582, -1.35));
+virgo.push( new THREE.Vector4 (-229.295, -101.33, -8.99576, -5.41));
+virgo.push( new THREE.Vector4 (-37.8925, -6.77861, 1.87601, 2.38));
+virgo.push( new THREE.Vector4 (-96.5948, -16.9301, 28.4999, 0.369));
+virgo.push( new THREE.Vector4 (-66.9683, -27.2416, 10.9805, 1.62));
+virgo.push( new THREE.Vector4 (-195.952, -39.7322, 30.2864, -0.575));
+virgo.push( new THREE.Vector4 (-35.4648, 1.75793, 0.430268, 3.4));
+virgo.push( new THREE.Vector4 (-45.8606, -38.6614, 10.2223, 2.51));
+virgo.push( new THREE.Vector4 (-248.489, -21.0087, 5.94371, -0.53));
+virgo.push( new THREE.Vector4 (-310.026, 31.7159, 25.0021, -0.871));
+virgo.push( new THREE.Vector4 (-57.465, -38.4651, 8.73479, 2.42));
+virgo.push( new THREE.Vector4 (-168.72, 6.78849, 25.3116, 0.523));
+virgo.push( new THREE.Vector4 (-183.794, -126.288, 11.341, 0.000276));
+virgo.push( new THREE.Vector4 (-187.987, -98.8989, 49.2797, 0.102));
+virgo.push( new THREE.Vector4 (-393.472, -129.648, 12.6003, -1.14));
+virgo.push( new THREE.Vector4 (-353.281, 15.0551, 38.1234, -0.541));
